f3ec17828f374542ca8b4d21b254f0eff8b2b192
agiledon <bruce.e.zhang@gmail.com>
2013-10-22 09:29:39 +0800

    24. do same thing for getFrequentRenterPoints() method

--- src/main/java/com/thoughtworks/refactor/martinfolwerbook/Movie.java
+++ src/main/java/com/thoughtworks/refactor/martinfolwerbook/Movie.java
@@ -42,15 +42,7 @@ public class Movie {
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        // add frequent renter points
-        int frequentRenterPoints = 0;
-        frequentRenterPoints++;
-        // add bonus for a two day new release rental
-        if ((this.getPriceCode() == NEW_RELEASE)
-                &&
-                daysRented > 1) {
-            frequentRenterPoints++;
-        }
-        return frequentRenterPoints;
+        return price.getFrequentRenterPoints(daysRented);
     }
+
 }
--- src/main/java/com/thoughtworks/refactor/martinfolwerbook/Price.java
+++ src/main/java/com/thoughtworks/refactor/martinfolwerbook/Price.java
@@ -4,4 +4,17 @@ public abstract class Price {
     public abstract int getPriceCode();
 
     public abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        // add frequent renter points
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if ((this.getPriceCode() == Movie.NEW_RELEASE)
+                &&
+                daysRented > 1) {
+            frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
 }
