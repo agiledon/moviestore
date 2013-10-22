8c34c94b3cf40c409e7f3826037e8b97c25f6b4d
agiledon <bruce.e.zhang@gmail.com>
2013-10-22 09:25:31 +0800

    22. remove unnecessary case for each subclass

--- src/main/java/com/thoughtworks/refactor/martinfolwerbook/RegularPrice.java
+++ src/main/java/com/thoughtworks/refactor/martinfolwerbook/RegularPrice.java
@@ -8,22 +8,9 @@ public class RegularPrice extends Price {
 
     public double getCharge(int daysRented) {
         double result = 0;
-        //determine amounts for aRental line
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
+        result += 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
         return result;
     }
 }
