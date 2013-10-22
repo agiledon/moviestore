5b00fe4f3203dfe80c8ba8a790764d8d840fcaa8
agiledon <bruce.e.zhang@gmail.com>
2013-10-22 09:20:29 +0800

    21. pull getCharge down to subclass

--- src/main/java/com/thoughtworks/refactor/martinfolwerbook/ChildrenPrice.java
+++ src/main/java/com/thoughtworks/refactor/martinfolwerbook/ChildrenPrice.java
@@ -5,4 +5,25 @@ public class ChildrenPrice extends Price {
     public int getPriceCode() {
         return Movie.CHILDREN;
     }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        //determine amounts for aRental line
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
