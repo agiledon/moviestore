b95440146c15e1d873c991f3034706d02ca3113f
yi <brucezhang@tom.com>
2020-10-29 14:34:43 +0800

    yi - move amountFor from customer to rental

--- src/main/java/com/thoughtworks/refactor/martinfolwerbook/Customer.java
+++ src/main/java/com/thoughtworks/refactor/martinfolwerbook/Customer.java
@@ -24,33 +24,17 @@ class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
 
-        for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+        for (Rental rental : rentals) {
+            double thisAmount = rental.amountFor();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
                     &&
-                    each.getDaysRented() > 1)
+                    rental.getDaysRented() > 1)
                 frequentRenterPoints++;
             //show figures
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
 
@@ -60,4 +44,5 @@ class Customer {
                 " frequent renter points";
         return result;
     }
+
 }
--- src/main/java/com/thoughtworks/refactor/martinfolwerbook/Rental.java
+++ src/main/java/com/thoughtworks/refactor/martinfolwerbook/Rental.java
@@ -16,4 +16,24 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    public double amountFor() {
+        double thisAmount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 }
