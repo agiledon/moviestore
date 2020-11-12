a32677ddf67786c9d5a421eead120af5884cf29d
zhangyi <brucezhang@tom.com>
2020-11-12 15:04:30 +0800

    done

--- src/main/java/com/thoughtworks/refactor/martinfolwerbook/model/Movie.java
+++ src/main/java/com/thoughtworks/refactor/martinfolwerbook/model/Movie.java
@@ -1,23 +1,12 @@
 package com.thoughtworks.refactor.martinfolwerbook.model;
 
 public class Movie {
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-    public static final int CHILDREN = 2;
-    private final PriceCode regularPriceCode = new RegularPriceCode();
-    private final PriceCode newReleasePriceCode = new NewReleasePriceCode();
-    private final PriceCode childPriceCode = new ChildrenPriceCode();
-
     private String title;
-    private int priceCodeType;
+    private final PriceCode priceCode;
 
     public Movie(String title, int priceCodeType) {
         this.title = title;
-        this.priceCodeType = priceCodeType;
-    }
-
-    public void setPriceCodeType(int arg) {
-        priceCodeType = arg;
+        priceCode = PriceCode.of(priceCodeType);
     }
 
     public String getTitle() {
@@ -26,31 +15,10 @@ public class Movie {
 
     // 差异式编程: Programming by Difference
     public double amountFor(int days) {
-        PriceCode priceCode = createPriceCode();
         return priceCode.amountFor(days);
     }
 
-    private PriceCode createPriceCode() {
-        PriceCode priceCode;
-        switch (priceCodeType) {
-            case REGULAR:
-                priceCode = new RegularPriceCode();
-                break;
-            case NEW_RELEASE:
-                priceCode = new NewReleasePriceCode();
-                break;
-            case CHILDREN:
-                priceCode = new ChildrenPriceCode();
-                break;
-            default:
-                priceCode = new DefaultPriceCode();
-        }
-        return priceCode;
-    }
-
     public int pointsFor(int days) {
-        PriceCode priceCode = createPriceCode();
         return priceCode.points(days);
     }
-
 }
--- src/main/java/com/thoughtworks/refactor/martinfolwerbook/model/PriceCode.java
+++ src/main/java/com/thoughtworks/refactor/martinfolwerbook/model/PriceCode.java
@@ -1,6 +1,23 @@
 package com.thoughtworks.refactor.martinfolwerbook.model;
 
 public abstract class PriceCode {
+    private static final int REGULAR = 0;
+    private static final int NEW_RELEASE = 1;
+    private static final int CHILDREN = 2;
+
+    static PriceCode of(int priceCodeType) {
+        switch (priceCodeType) {
+            case REGULAR:
+                return new RegularPriceCode();
+            case NEW_RELEASE:
+                return new NewReleasePriceCode();
+            case CHILDREN:
+                return new ChildrenPriceCode();
+            default:
+                return new DefaultPriceCode();
+        }
+    }
+
     public abstract double amountFor(int days);
 
     public int points(int days) {
